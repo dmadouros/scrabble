923ddef6f7bf22cc420d6ac7be73a9c4bfefdf07
David Madouros <dmadouros@comcast.net>
2014-08-24 22:15:21 -0600

    Move SCORES constant from spec to implementation

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,7 +1,7 @@
 require "scrabble/version"
 
 module Scrabble
-  LETTER_SCORES = {
+  SCORES = {
     "A" => 1, "B" => 3, "C" => 3, "D" => 2,
     "E" => 1, "F" => 4, "G" => 2, "H" => 4,
     "I" => 1, "J" => 8, "K" => 5, "L" => 1,
@@ -14,6 +14,6 @@ module Scrabble
   def self.score(word)
     return 0 if word.nil? || word.empty?
 
-    LETTER_SCORES[word.upcase]
+    SCORES[word.upcase]
   end
 end
