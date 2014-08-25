07c790ce0f937b9a8b64f53d809b9314930c70e9
Kim D Barnes <kimdbarnes@gmail.com>
2014-08-25 07:23:42 -0600

    more functional solution

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,90 +1,37 @@
-require "scrabble/version"
+require 'scrabble/version'
 
 module Scrabble
+  SCORES = {
+    'A' => 1, 'B' => 3, 'C' => 3, 'D' => 2,
+    'E' => 1, 'F' => 4, 'G' => 2, 'H' => 4,
+    'I' => 1, 'J' => 8, 'K' => 5, 'L' => 1,
+    'M' => 3, 'N' => 1, 'O' => 1, 'P' => 3,
+    'Q' => 10, 'R' => 1, 'S' => 1, 'T' => 1,
+    'U' => 1, 'V' => 4, 'W' => 4, 'X' => 8,
+    'Y' => 4, 'Z' => 10
+  }
+
   def self.score(word)
-    Word.new(word).score
+    return 0 if word.nil? || word.empty?
+
+    word.upcase.chars.map do |char|
+      SCORES.fetch(char)
+    end.reduce(:+)
   end
 
   def self.highest_score_from(words)
-    words.map { |word| Word.new(word) }.max.text
-  end
-
-  class Word
-    SCORES = {
-      "A" => 1, "B" => 3, "C" => 3, "D" => 2,
-      "E" => 1, "F" => 4, "G" => 2, "H" => 4,
-      "I" => 1, "J" => 8, "K" => 5, "L" => 1,
-      "M" => 3, "N" => 1, "O" => 1, "P" => 3,
-      "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
-      "U" => 1, "V" => 4, "W" => 4, "X" => 8,
-      "Y" => 4, "Z" => 10
-    }
-
-    attr_reader :text
-
-    def initialize(text)
-      @text = text
-    end
-
-    def score
-      return 0 unless present?
-
-      chars.map { |char| SCORES[char] }.reduce(:+)
-    end
-
-    def length
-      text.length
-    end
-
-    private
-
-    def <=>(other)
-      WordComparator.new(self, other).compare
-    end
-
-    def chars
-      text.upcase.chars
-    end
-
-    def present?
-      !text.nil? && !text.empty?
+    words.max do |word1, word2|
+      score_comparison = score(word1) <=> score(word2)
+      score_comparison == 0 ? length_comparison(word1, word2) : score(word1) <=> score(word2)
     end
   end
 
-  class ScrabbleComparator
-    def initialize(this, that)
-      @this = this
-      @that = that
-    end
-
-    private
-
-    attr_reader :this, :that
+  def self.length_comparison(word1, word2)
+    all_tiles = 7
+    return 1 if word1.length == all_tiles
+    return -1 if word2.length == all_tiles
+    word2.length <=> word1.length
   end
 
-  class WordComparator < ScrabbleComparator
-    def compare
-      ScoreComparator.new(this, that).tie? ? LengthComparator.new(this, that).compare : ScoreComparator.new(this, that).compare
-    end
-  end
-
-  class ScoreComparator < ScrabbleComparator
-    def tie?
-      compare == 0
-    end
-
-    def compare
-      this.score <=> that.score
-    end
-  end
-
-  class LengthComparator < ScrabbleComparator
-    MAX_LETTERS = 7
-
-    def compare
-      return 1 if this.length == MAX_LETTERS
-      return -1 if that.length == MAX_LETTERS
-      that.length <=> this.length
-    end
-  end
+  private_class_method :length_comparison
 end
